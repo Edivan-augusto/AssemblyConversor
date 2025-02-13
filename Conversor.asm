0e651569444fb7728e9585c6847950a898c7433b
Edivan augusto <94809334+Edivan-augusto@users.noreply.github.com>
2025-02-13 07:57:14 -0300

    Update Conversor.asm
    Adicionando comentarios

--- Conversor.asm
+++ Conversor.asm
@@ -15,41 +15,43 @@ section .text
     global _start
 
 _start:
-
+    ; Imprime a primeira mensagem no console
     mov rax , 1
     mov rdi , 1
     mov rsi , msg1
     mov rdx , len1
     syscall
 
+    ; Imprime a Segunda mensagem no console
     mov rax , 1
     mov rdi , 1
     mov rsi , msg2
     mov rdx , len2
     syscall
 
+    ; Faz a solicitação do input do usuario e o armazena , eu usei 50 bytes por pura convenção mas poderia ser usado 1 só ( por conseguirmos converter apenas 1 digito )
     mov rax , 0
     mov rdi , 0
     mov rsi , input
     mov rdx , 50
     syscall
 
+    ; Move o digito para o al (menor parte do EAX , e o converte para numero real), e move 2 para o bl para a divisão
     mov al , [input]
     sub al , 48
-    
     mov bl , 2
-    
+
+    ; Limpa o input para armazenar o valor de conversão
     mov byte [input] , 0
 
+    ; Move 0 para o rsi que sera usado como controle no loop
     mov rsi , 0
-    mov byte [input + rsi] , ah
     
     
 to_convert:
-
+    ; Aqui é realizado o Loop para conversão , movendo os valores para a variavel input , a cada iteração ele move o endereço para frente entao cada numero fica em 1 byte
     
     div bl
-    
     add ah , 48
     mov byte [input + rsi] , ah
     
@@ -60,14 +62,14 @@ to_convert:
     
 
 exit:
-
+    ; Realiza a saida no console da conversão
     mov rax , 1
     mov rdi , 1
     mov rsi , input
     mov rdx , 50
     syscall
 
-    
+    ; Finaliza o programa 
     mov rax , 60
     xor rdi , rdi
     syscall
